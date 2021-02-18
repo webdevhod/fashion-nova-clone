6a879ae23071c1438500cf8dbcd4596e7414f4c5
webdevhod <webdevhod@gmail.com>
2021-02-18 12:15:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Fashion Nova Clone - 87 Lux
+
+Run npm install to install all the dependencies and then npm run dev to start the next server
---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import '../styles/style.scss'
+import Layout from '../components/layout'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+			<Component {...pageProps} />
+		</Layout>
+  )
+}
+
+export default MyApp;
